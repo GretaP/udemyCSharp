d53c40493474da7f90177c0ab0bf11f31753077c
Greta Parks <gretaparks@gmail.com>
2019-11-20 16:45:47 -0700

    first commit

---
+++ Practice problems/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Practice_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Input a # between 1 and 10: ");
+            //var answer = Convert.ToInt64(Console.ReadLine());
+            //Console.WriteLine("Your input was: {0}", answer);
+            //if (0 < answer && answer <11) { Console.WriteLine("Bingo"); }
+            //else Console.WriteLine("You don't follow instructions");
+
+            //< summary >
+            //Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            //Display the result on the console.
+            //</ summary >
+            //int count = 0;
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0) count++;
+            //}
+            //Console.WriteLine(count);
+
+            /*2- Write a program and continuously ask the user to enter a number or "ok" to exit. 
+             * Calculate the sum of all the previously entered numbers and display it on the console.*/
+
+            //var sums = 0;
+            //while (true) {
+            //    Console.WriteLine("ok to exit, or enter a number to be added");
+            //    var entry = Console.ReadLine();
+            //    if (entry == "ok") break;
+            //    sums += int.Parse(entry);
+            //}
+            //Console.WriteLine(sums);
+
+
+            /*2- Write a program and ask the user to enter their name. 
+             * Use an array to reverse the name and then store the result in a new string. 
+             * Display the reversed name on the console.*/
+
+            //Console.WriteLine("Enter your name");
+            //var name = Console.ReadLine().ToCharArray();
+            //Array.Reverse(name);
+            //string answer = new string(name);
+            //Console.WriteLine(answer);
+
+
+            // WORKING WITH TEXT
+
+            //1 - Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //Work out if the numbers are consecutive.
+            //example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+            const string input = "5-6-5-8-9";
+            var num = Array.ConvertAll(input.Split("-"), int.Parse);
+            foreach (var n in num)
+            {
+                Console.WriteLine(n);
+                Console.WriteLine(n.GetType());
+            }
+
+            var increment = (num[0] < num[1]) ? 1 : -1;
+            var consecutive = true;
+
+            Console.WriteLine(num.Length);
+            for (var i = 0; i < num.Length-2; i++)
+            {
+                if (num[i]+increment != num[i+1])
+                {
+                    consecutive = false;
+                    break;
+                }
+            }
+            Console.Write(consecutive);
+
+
+        }
+    }
+}
---
+++ PracticeProbText/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PracticeProbText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WORKING WITH TEXT
+
+            //1 - Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //Work out if the numbers are consecutive.
+            //example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+            Console.WriteLine("Input numbers in the format x-x-x-x, and the program will tell you if they are consecutive (increasing/decreasing by 1 at a time) or not");
+            var input = Console.ReadLine();
+            //const string input = "5-6-5-8-9";
+            Console.WriteLine($"Your input is a set of {(ConsecutiveCheck(input) ? "consecutive" : "non-consecutive")} numbers");
+
+            //2 - Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates.
+            //If so, display "Duplicate" on the console.
+
+
+        }
+
+        public static int[] HyphenStringtoIntArray(string x)
+        {
+            return Array.ConvertAll(x.Split("-"), int.Parse);
+        }
+
+        public static bool ConsecutiveCheck(string input)
+        { 
+            //returns true if string input in format #-#-#-# is consecutive
+            var num = Array.ConvertAll(input.Split("-"), int.Parse);
+            var increment = (num[0] < num[1]) ? 1 : -1;
+            for (var i = 0; i < num.Length-1; i++)
+            {
+                if (num[i] + increment != num[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
