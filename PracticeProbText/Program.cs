2855abb81013fdd8c907f670a1345ecd788c31a1
Greta Parks <gretaparks@gmail.com>
2019-11-22 11:09:38 -0700

    Adds more problems in the same file

--- PracticeProbText/Program.cs
+++ PracticeProbText/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace PracticeProbText
 {
@@ -9,7 +10,9 @@ namespace PracticeProbText
             // WORKING WITH TEXT
             // Inputs do not need to validated
            // Exercise1();
-            Exercise2();
+            //Exercise2();
+            //Exercise3();
+            Exercise4();
 
 
 
@@ -38,21 +41,83 @@ namespace PracticeProbText
             if (string.IsNullOrEmpty(duplicateInput)) Environment.Exit(-1);
             DuplicateCheck(duplicateInput);
         }
+
+        //public void Exercise3()
+        //{
+
+        //    //Write a program and ask the user to enter a time value in the 24 - hour time format(e.g. 19:00).
+        //    //A valid time should be between 00:00 and 23:59.
+        //    //If the time is valid, display "Ok"; otherwise, display "Invalid Time".
+        //    //If the user doesn't provide any values, consider it as invalid time.
+
+        //    Console.WriteLine("Enter a valid time between 00:00 and 23:59.  This will tell you if it is formatted properly or not");
+        //    var input = Console.ReadLine();
+        //    Console.WriteLine(TimeValidation(input) ? "Ok" : "Invalid Time");
+        //    {
+
+        //    }
+        //}
+
+        public static void Exercise4()
+        {
+            // Write a program and ask the user to enter a few words separated by a space. Use the words to 
+            // create a variable name with PascalCase convention. For example, if the user types:
+            // "number of students", display "NumberOfStudents". Make sure the program is not dependent on 
+            // the casing of the input. So if the input is "NUMBER OF STUDENTS", it should still display 
+            // "NumberOfStudents". If the user doesn't supply any words, display "Error".
+
+
+            Console.WriteLine("enter a string, and I will pascal case it");
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Error - you didn't type anything");
+                return;
+            }
+
+            var pascalCase = "";
+            var words = input.Split(' ');
+            foreach (var w in words)
+            {
+                var pascalWord = Char.ToUpper(w[0]) + w.ToLower().Substring(1);
+                pascalCase += pascalWord;
+            }
+            Console.WriteLine(pascalCase);
+
+        }
+
+
+        //public static bool TimeValidation (string s)
+        //{
+        //    NOT DONE
+        //    if (string.IsNullOrEmpty(s)) return false;
+        //    var parts = s.Split(":");
+        //    if (parts.Length != 2) return false;
+        //    foreach (var p in parts)
+        //    {
+        //        if (p.Length != 2) return false;
+        //    }
+        //    return true;
+
+        //}
+
         public static int[] HyphenStringtoIntArray(string x)
         {
             return Array.ConvertAll(x.Split("-"), int.Parse);
         }
 
-        public static bool DuplicateCheck(string input)
+        public static void DuplicateCheck(string input)
         {
             var num = HyphenStringtoIntArray(input);
             Array.Sort(num);
-            foreach (var n in num)
+            for (var i=0; i<num.Length-1; i++)
             {
-                //if (n ==) return true;
+                if (num[i] == num[i + 1])
+                {
+                    Console.WriteLine("Duplicate");
+                    break;
+                };
             }
-
-            return false;
         }
 
         public static bool ConsecutiveCheck(string input)
