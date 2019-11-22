414db4d4ed00fc866a7940438495ba3c4264d324
Greta Parks <gretaparks@gmail.com>
2019-11-22 13:33:51 -0700

    Changes exercise 3

--- PracticeProbText/Program.cs
+++ PracticeProbText/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace PracticeProbText
 {
     class Program
     {
+        /// <summary>
+        /// Working with Text: Udemy Exercises.  Inputs do not need to be validated
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // WORKING WITH TEXT
-            // Inputs do not need to validated
-           // Exercise1();
+            //Exercise1();
             //Exercise2();
-            //Exercise3();
+            Exercise3();
             //Exercise4();
-            Exercise5();
+            //Exercise5();
         }
 
         //1 - Write a program and ask the user to enter a few numbers separated by a hyphen.
         //Work out if the numbers are consecutive.
         //example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+        /// <summary>
+        /// Input numbers in the format x-x-x-x, and the program will tell you if they are consecutive (increasing/decreasing by 1 at a time) or not
+        /// </summary>
         public static void Exercise1()
         {
             Console.WriteLine("Input numbers in the format x-x-x-x, and the program will tell you if they are consecutive (increasing/decreasing by 1 at a time) or not");
@@ -32,6 +39,9 @@ namespace PracticeProbText
         /* 2 - Write a program and ask the user to enter a few numbers separated by a hyphen.
         If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates
         If so, display "Duplicate" on the console. */
+        /// <summary>
+        /// Input numbers in the format x-x-x-x, and program will check to see if there are duplicates.
+        /// </summary>
         public static void Exercise2()
         {
 
@@ -41,36 +51,47 @@ namespace PracticeProbText
             DuplicateCheck(duplicateInput);
         }
 
-/*        public void Exercise3()
+        /* 3. Write a program and ask the user to enter a time value in the 24 - hour time format(e.g. 19:00).
+        A valid time should be between 00:00 and 23:59.
+        If the time is valid, display "Ok"; otherwise, display "Invalid Time".
+        If the user doesn't provide any values, consider it as invalid time.*/
+        public static void Exercise3()
         {
-
-            //Write a program and ask the user to enter a time value in the 24 - hour time format(e.g. 19:00).
-            //A valid time should be between 00:00 and 23:59.
-            //If the time is valid, display "Ok"; otherwise, display "Invalid Time".
-            //If the user doesn't provide any values, consider it as invalid time.
-
-            Console.WriteLine(
-                "Enter a valid time between 00:00 and 23:59.  This will tell you if it is formatted properly or not");
+            Console.WriteLine("Enter a valid time between 00:00 and 23:59.  This will tell you if it is formatted properly or not");
             var input = Console.ReadLine();
             Console.WriteLine(TimeValidation(input) ? "Ok" : "Invalid Time");
-            {
 
-            }
-        }*/
+        }
 
-/*
+        /// <summary>
+        /// Takes an entered string and returns true if it is a time in ##:## format between 00:00 and 23:59
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static bool TimeValidation(string s)
         {
-            NOT DONE
             if (string.IsNullOrEmpty(s)) return false;
             var parts = s.Split(":");
             if (parts.Length != 2) return false;
-            foreach (var p in parts)
+            if (parts.Any(p => p.Length != 2)) return false;
+            //foreach (var p in parts)
+            //{
+            //    if (p.Length != 2) return false;
+            //}
+
+            try
             {
-                if (p.Length != 2) return false;
+                var hours = int.Parse(parts[0]);
+                var minutes = int.Parse(parts[1]);
+                if (minutes < 0 || minutes > 59) return false ;
+                if (hours <0 || hours >24) return false;
+            }
+            catch
+            {
+                return false;
             }
             return true;
-        }*/
+        }
 
 
         // Write a program and ask the user to enter a few words separated by a space. Use the words to 
@@ -109,6 +130,8 @@ namespace PracticeProbText
             Console.WriteLine("Enter and English word");
             var input = Console.ReadLine();
             var vowelCount = 0;
+            if (string.IsNullOrEmpty(input)) Environment.Exit(-1);
+            //next time take the input and put it to lower, rather than writing out all the vowels >.>
             List<string> vowels = new List<string> {"a", "e", "i", "o", "u", "A","E","I","O","U"};
             foreach (var n in input)
             {
