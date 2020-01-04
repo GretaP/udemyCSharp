92fe6f3b06664530f7eed07b9874218fc2319ac7
Greta Parks <gretaparks@gmail.com>
2020-01-04 12:42:46 -0700

    Adds IntermediateA

---
+++ IntermediateSetA/Program.Stopwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace IntermediateSet1
+{
+    partial class Program
+    {
+        public class Stopwatch
+        {
+            private DateTime _begin = default;
+            private DateTime _end = default;
+
+            public void Start()
+            {
+                //Checks if user has already started the stopwatch, otherwise starts
+                try
+                {
+                    if ((_begin) != default) throw new InvalidOperationException("Stopwatch already running: Please stop the stopwatch or type reset to reset it");
+                    _begin = DateTime.Now;
+                    Console.WriteLine("Stopwatch is starting");
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            public void Stop()
+            {
+                try
+                {
+                    //Checks if user has already started the stopwatch, if not does not stop.  Otherwise stops and prints duration
+                    //Duration is type TimeSpan
+                    if ((_begin) == default) throw new InvalidOperationException("Stopwatch not running: Please start the stopwatch");
+                    _end = DateTime.Now;
+                    Console.WriteLine("Stopwatch stopped.");
+                    var duration = _end - _begin;
+                    Console.WriteLine($"Stopwatch was on for {duration}. \n(Note: duration is of type: {duration.GetType()})");
+                    _begin = default;
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            public void Reset()
+            {
+                _begin = default;
+                Console.WriteLine("Stopwatch reset");
+            }
+        }
+    }
+}
---
+++ IntermediateSetA/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IntermediateSet1
+{
+    partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            //starts a stopwatch program
+            var stopwatch = new Stopwatch();
+            Console.WriteLine("Welcome to this simple stopwatch. \nType start to start or stop to stop the stopwatch. \nCheers\n");
+            while (true)
+            {
+                var input = Console.ReadLine()?.ToLower();
+                switch (input)
+                {
+                    case "start": 
+                        stopwatch.Start();
+                        break;
+                    case "stop": 
+                        stopwatch.Stop();
+                        break;
+                    case "reset":
+                        stopwatch.Reset();
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        Console.WriteLine("Your input was invalid.  type start or stop to use the stopwatch");
+                        break;
+                }
+            }
+            //end of stopwatch program
+
+        }
+    }
+}
