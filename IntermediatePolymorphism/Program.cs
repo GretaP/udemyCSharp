0e6e01a2e34c8a1abf1baa37f1cb40501c11418c
Greta Parks <gretaparks@gmail.com>
2020-01-06 12:58:43 -0700

    Adds next exercise: instructions only

---
+++ IntermediatePolymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntermediatePolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
