5abb730bb8d57b9085cbaca0b8732ee24700c883
Greta Parks <gretaparks@gmail.com>
2020-01-06 13:50:59 -0700

    Working on Polymorphism exercise

---
+++ IntermediatePolymorphism/DbConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace IntermediatePolymorphism
+{
+    public abstract class DbConnection
+    {
+        //public string ConnectionString { get; set; }
+        //public TimeSpan Timeout { get; set; }
+
+        public abstract void Open();
+        public abstract void Close();
+
+/*        public DbConnection(string connectionString)
+        {
+           // if (connectionString.IsNull)
+
+        }*/
+    }
+
+    public class SqlConnection : DbConnection
+    {
+        public override void Open()
+        {
+            Console.WriteLine("SQL connection opened (let's pretend!) {0}");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("SQL Closed Let's pretend");
+        }
+    }
+}
