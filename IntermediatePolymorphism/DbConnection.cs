cd0936e96f444252357fa485d01c57baae7d38a7
Greta Parks <gretaparks@gmail.com>
2020-01-06 18:11:23 -0700

    Working

--- IntermediatePolymorphism/DbConnection.cs
+++ IntermediatePolymorphism/DbConnection.cs
@@ -7,29 +7,50 @@ namespace IntermediatePolymorphism
 {
     public abstract class DbConnection
     {
-        //public string ConnectionString { get; set; }
-        //public TimeSpan Timeout { get; set; }
+        public DbConnection(string connectionString)
+        {
+            if (String.IsNullOrEmpty(connectionString)) throw new InvalidOperationException("You can not connect with and empty connection string; connection failed.");
+            ConnectionString = connectionString;
+        }
 
+        protected string ConnectionString { get; }
+        protected TimeSpan TimeOut { get; set; }
+        
         public abstract void Open();
         public abstract void Close();
-
-/*        public DbConnection(string connectionString)
-        {
-           // if (connectionString.IsNull)
-
-        }*/
     }
 
     public class SqlConnection : DbConnection
     {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+
         public override void Open()
         {
-            Console.WriteLine("SQL connection opened (let's pretend!) {0}");
+            Console.WriteLine("SQL connection opened (let's pretend!) {0}.  TimeoutValue is: {1}", ConnectionString, TimeOut);
         }
 
         public override void Close()
         {
             Console.WriteLine("SQL Closed Let's pretend");
+        }
+    }
+
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Oracle connection opened (let's pretend!) {0}", ConnectionString);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Oracle Closed Let's pretend");
         }
     }
 }
--- IntermediatePolymorphism/Program.cs
+++ IntermediatePolymorphism/Program.cs
@@ -6,7 +6,25 @@ namespace IntermediatePolymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+/*            DbConnection connectionSql = new SqlConnection("nyan kittie");
+            connectionSql.Open();
+            DbConnection connectionOracle = new OracleConnection("Tell us the way of the world oh oracle");
+            connectionOracle.Open();
+            connectionOracle.Close();*/
+
+            try
+            {
+
+                var connectionEmpty = new SqlConnection("");
+                //var connectionEmpty = new SqlConnection();
+                connectionEmpty.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
         }
     }
 }
