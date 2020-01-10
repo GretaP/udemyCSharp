4dcbf81411d663631c0a392abe71116c9f189910
Greta Parks <gretaparks@gmail.com>
2020-01-10 08:08:12 -0700

    Working

---
+++ IntermediateInterfaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IntermediateInterfaces
+{
+    public interface IActivity
+    {
+        void Execute();
+    }
+    
+    public class VideoUpload : IActivity
+    {
+        public void Execute()
+        {
+            Console.WriteLine("We have uploaded the video");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var testing = new VideoUpload();
+            testing.Execute();
+        }
+    }
+}
