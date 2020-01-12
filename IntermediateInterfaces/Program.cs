7c2eecd3a371177889e6b27e876371804df236a3
Greta Parks <gretaparks@gmail.com>
2020-01-11 22:58:52 -0700

    Fixing silly typo

--- IntermediateInterfaces/Program.cs
+++ IntermediateInterfaces/Program.cs
@@ -20,47 +20,42 @@ namespace IntermediateInterfaces
                     Console.WriteLine(
                         "------------\nMenu: \nV to add VideoUpload to workflow \nC to add change video record to workflow \nE to add email to workflow.  \nS to see your current workflow  \nN to create a new workflow from scratch \nR to run workflow \nesc to end program \n-----------------");
                     var key = Console.ReadKey(true);
+                    Console.Clear();
+
                     switch (key.Key)
                     {
                         case ConsoleKey.Escape:
                             return;
                         case ConsoleKey.V:
                             workFlow.Add(new VideoUpload());
-                            Console.Clear();
                             Console.WriteLine("Added VideoUpload to workflow.");
                             break;
                         case ConsoleKey.C:
                             workFlow.Add(new VideoRecordChange());
-                            Console.Clear();
                             Console.WriteLine("Added VideoRecordChange to workflow.");
                             break;
                         case ConsoleKey.E:
                             workFlow.Add(new SendEmail());
-                            Console.Clear();
                             Console.WriteLine("Added SendEmail to workflow.");
                             break;
                         case ConsoleKey.N:
                             workFlow = new WorkFlowList();
-                            Console.Clear();
                             Console.WriteLine("Cleared the workflow");
                             break;
                         case ConsoleKey.S:
                             var getList = workFlow.GetWorkflow();
-                            Console.Clear();
                             foreach (var activity in getList)
                             {
                                 Console.WriteLine(activity);
                             }
                             break;
                         case ConsoleKey.R:
-                            Console.Clear();
                             Console.WriteLine("-----------------------\nRunning workflow");
                             var engine = new WorkflowEngine(workFlow.GetWorkflow());
                             engine.Run();
                             Console.WriteLine("-----------------------");
                             break;
                         default:
-                            Console.Clear();
                             Console.WriteLine("Invalid entry - try again");
                             break;
                     }
